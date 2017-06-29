1f0cee072f4617845eba5c30bfb62ca1431dad75
FXQeng <3111019794@qq.com>
2017-06-29 21:42:46 +0800

    实现点击随机测试进入考试界面

---
+++ app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.jkbd;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Who on 2017/6/29.
+ */
+
+class ExamActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_exam);
+    }
+}
--- app/src/main/java/com/example/administrator/jkbd/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.jkbd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+
+    public void exit(View view) {
+        finish();
     }
 }
