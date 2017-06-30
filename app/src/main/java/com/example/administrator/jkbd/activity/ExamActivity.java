605c155a877b505a018c8f2d91fa1ac1964dd507
FXQeng <3111019794@qq.com>
2017-06-30 10:11:55 +0800

    新建了一个utils包，并添加了OKHttpUtils

--- app/src/main/java/com/example/administrator/jkbd/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/ExamActivity.java
@@ -1,18 +1,20 @@
-package com.example.administrator.jkbd;
+package com.example.administrator.jkbd.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.administrator.jkbd.R;
+
 /**
  * Created by Who on 2017/6/29.
  */
 
-class ExamActivity extends AppCompatActivity {
+public class ExamActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
     }
 }
--- app/src/main/java/com/example/administrator/jkbd/activity/SplashActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/SplashActivity.java
@@ -1,10 +1,12 @@
-package com.example.administrator.jkbd;
+package com.example.administrator.jkbd.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.jkbd.R;
 
 /**
  * Created by Administrator on 2017/6/28.
@@ -20,7 +22,6 @@ public class SplashActivity extends AppCompatActivity{
     CountDownTimer mCountDownTimer  = new CountDownTimer(2000,1000) {
      @Override
        public void onTick(long millisUntilFinished) {
-
      }
       @Override
       public void onFinish() {
