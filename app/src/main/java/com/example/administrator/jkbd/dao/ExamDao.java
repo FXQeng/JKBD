364735ddc93fcce4bce3800921021f1d7035d543
FXQeng <3111019794@qq.com>
2017-07-02 22:34:49 +0800

    添加biz和dao包进行开发

---
+++ app/src/main/java/com/example/administrator/jkbd/dao/ExamDao.java
@@ -0,0 +1,28 @@
+package com.example.administrator.jkbd.dao;
+
+import android.content.Intent;
+import android.util.Log;
+
+import com.example.administrator.jkbd.ExamApplication;
+import com.example.administrator.jkbd.bean.ExamInfo;
+import com.example.administrator.jkbd.bean.Question;
+import com.example.administrator.jkbd.bean.Result;
+import com.example.administrator.jkbd.utils.OkHttpUtils;
+import com.example.administrator.jkbd.utils.ResultUtils;
+
+import java.util.List;
+
+/**
+ * Created by Who on 2017/7/2.
+ */
+
+public class ExamDao implements IExamDao {
+    @Override
+    public void loadExamInfo() {
+
+    }
+    @Override
+    public void loadQuestionLists() {
+
+    }
+}
