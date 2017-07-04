608c9df9018ec3ec83eb12a1def23234d6321982
FXQeng <3111019794@qq.com>
2017-07-04 13:17:52 +0800

    考试界面进行答题时初始化选项

--- app/src/main/java/com/example/administrator/jkbd/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/ExamActivity.java
@@ -180,9 +180,15 @@ public class ExamActivity extends AppCompatActivity {
             } else {
                 mImageView.setVisibility(View.GONE);
             }
+            resetOptions();
         }
     }
 
+    private void resetOptions() {
+        for(CheckBox cb:cbs){
+            cb.setChecked(false);
+        }
+    }
 
 
     private void showData(ExamInfo examInfo) {
