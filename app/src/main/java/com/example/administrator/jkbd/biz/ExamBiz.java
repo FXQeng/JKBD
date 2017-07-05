bb6124a47367b67a02f53d3b30253c65a877b83f
FXQeng <3111019794@qq.com>
2017-07-05 11:09:16 +0800

    修改commitExam方法

--- app/src/main/java/com/example/administrator/jkbd/biz/ExamBiz.java
+++ app/src/main/java/com/example/administrator/jkbd/biz/ExamBiz.java
@@ -68,11 +68,11 @@ public class ExamBiz implements IExamBiz {
 
     @Override
     public int commitExam() {
-        int s=0;
-        for(Question exam:examList){
-            String userAnswer=exam.getUserAnswer();
-            if(userAnswer!=null&&!userAnswer.equals("")){
-                if(exam.getAnswer().equals("")){
+        int s = 0;
+        for(Question exam : examList){
+            String userAnswer = exam.getUserAnswer();
+            if(userAnswer!=null && !userAnswer.equals("")){
+                if(exam.getAnswer().equals(userAnswer)){
                     s++;
                 }
             }
